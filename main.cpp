ea28b19f93ea8ba4a282fc1f8742510b226e96e5
mohammad <benyaminbusiness81@gmail.com>
2021-09-18 21:51:21 +0430

    full version of counting charecters with correction and bug fixed

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 int main(){
     string name;
-    int counter = 0;
+    int counter = 0, spacecounter = 0;
     cout<<"what is the input string? ";
-    cin>>name;
-    for(int i = 1; i<= name.size(); i++){
+    getline(cin, name);
+    while(true){
+        if(name.empty()){
+            cout<<"you have to write something!"<<endl;
+            getline(cin, name);
+        }
+        else{
+            break;
+        }
+    }
+    for(int i = 1; i <= name.size(); i++){
         counter++;
     }
+    for(int i = 0; i < name.size(); i++){
+        if(isspace(name.at(i))){
+           spacecounter++;
+        }
+    }
+    counter -= spacecounter;
     cout<<name<<" has "<<counter<<" charecters";
 }
